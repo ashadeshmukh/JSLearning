1a3a517f29f01ce86f8c43e24b451c52b93d8acb
ashadeshmukh <ambhore.asha26@gmail.com>
2023-02-16 14:27:03 +0530

    updated file variableAssign.js
    updated file variableAssign.js

---
+++ variableAssign.js
@@ -0,0 +1,48 @@
+var myFullName;
+console.log("........1.DECLARE A VARIABLE WITH NAME.........");
+console.log(myFullName);
+
+//var myFullName;
+console.log("Full Name: Asha Deshmukh");
+//myFullName = "Asha Deshmukh";//initialization
+//console.log(myFullName);//updated variables 
+myFullName = "Full Name with Middle Name:Asha Dinesh Deshmukh";
+console.log(myFullName);
+
+var myLove;
+console.log("......2.DECLARE A VARIABLE WITH myLove ........");
+myLove= "I love only JS";
+console.log(myLove);
+
+console.log("........3.CREATE A VARIABLES FOR FROM CONTROLS AS LIST BELOW..................");
+var firstName;
+firstName="First Name: Asha";
+console.log(firstName);
+
+var lastName;
+lastName = "Last Name:Deshmukh";
+console.log(lastName);
+
+var age = "Age:30";
+console.log(age);
+
+var address = "Address:Aurangbad";
+console.log(address);
+
+var pinCode = "Pin_Code:431001";
+console.log(pinCode);
+
+var gender = "Gender:Female";
+console.log(gender);
+
+var isMarried = "IsMarried: Yes";
+console.log(isMarried);
+
+console.log(".................4.Declare and initlization in same line.................");
+console.log("First Name:Asha, Last Name:Deshmukh, Age:30, Address:Aurangbad, Pin_Code:431001, Gender:Female, Is_Married:Yes");
+
+
+
+
+
+
