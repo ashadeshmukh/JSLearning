f060fff2412ef9c49d419b8c91c775fd0deed599
ashadeshmukh <ambhore.asha26@gmail.com>
2023-02-16 08:45:09 +0530

    updated file helloWorld.js
    updated file helloWorld.js

---
+++ helloWorld.js
@@ -0,0 +1,26 @@
+//Personal details
+console.log("ASHA DESHMUKH");
+console.log("30");
+console.log("Aurangbad");
+
+var fullName= "ASHA DESHMUKH";
+console.log(fullName);
+console.log(fullName);
+
+var city; //variable declaration 
+console.log(city);
+city = "Aurangbad"; //variable initialization
+console.log(city);
+city = "surat"; //variable update
+console.log(city);
+
+var age = "30";//variable declaration and initialization in same line
+age = 31;
+age = 32;
+console.log(age);
+
+var university; //declaration
+university = "SRTMU"; //initialization
+console.log(university);
+
+var university = "SRTMU";
