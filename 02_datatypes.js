31b98b73a267334b4e736f7fa5b51e3b1d189c02
ashadeshmukh <ambhore.asha26@gmail.com>
2023-02-18 13:18:39 +0530

    updated file 02_datatypes.js file
    updated file 02_datatypes is file

---
+++ 02_datatypes.js
@@ -0,0 +1,33 @@
+var myName = "Asha Deshmukh";
+var fatherName = "Anandrao Ambhore";
+
+var age = 31;
+var weight = 50.2;
+
+var isMarried = true;
+var isLazy = false;
+
+var typeofmyName = typeof myName;
+
+console.log("Typeof variable myName:", typeofmyName);
+console.log("typeof variable fatherName", typeof fatherName);
+console.log("typeof variable AGE", typeof age);
+console.log("typeof variable isMarried", typeof isMarried);
+console.log("typeof variable isLazy",typeof isLazy);
+
+var graduation;
+console.log("value of graduation variable is:", graduation);
+console.log("typeof variable graduation", typeof graduation);
+
+var city = null;
+console.log("value of city variable is:", city);
+console.log("typeof variable city", typeof city);
+
+console.log("Swapping variables");
+var num1 = 100;
+var num2 = 200;
+
+var temp = num1;
+num1 = num2;
+num2 = temp;
+console.log( num1, num2);
