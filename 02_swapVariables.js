ffeae6c443f30957f8ba352b093a5fa0f45c9b12
ashadeshmukh <ambhore.asha26@gmail.com>
2023-02-18 14:44:53 +0530

    updated swapVariables.js file
    updated file swapVariables.js

---
+++ 02_swapVariables.js
@@ -0,0 +1,28 @@
+console.log('..............swaping Variables..........');
+var sweety = "sweety";
+var cuties = "cuties";
+
+//var temp = sweety;
+sweety = sweety;
+cuties = cuties;
+console.log('sweety =sweety');
+console.log('cuties =cuties');
+
+var temp = sweety;
+sweety = cuties;
+cuties = temp;
+console.log(sweety,cuties);
+
+console.log('............swaping three variables............');
+var num1 = 100;
+var num2 = 200;
+var num3 = 300;
+
+//swapping variables
+var temp = num1;
+num1 = num2;
+num2 = num3;
+num3 = temp;
+console.log(num1,num2,num3);
+
+
