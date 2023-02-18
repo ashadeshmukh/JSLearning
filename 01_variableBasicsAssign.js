7ded5b996768238f2dc2aa275e7777b30e38d554
ashadeshmukh <ambhore.asha26@gmail.com>
2023-02-18 13:15:10 +0530

    updated file 01_variableBasicsAssign.js
    updated file 01_variableBasicsAssign.js

---
+++ 01_variableBasicsAssign.js
@@ -0,0 +1,37 @@
+var mobileNumber;
+var pinCode;
+var email;
+var isMarried;
+
+var mobileNumber = 7875547094;
+var pinCode = '453632';
+var email = 'ambhore.asha26@gmail.com';
+var isMarried = true;
+
+var typeofmobileNumber = typeof mobileNumber;
+var typeofpinCode = typeof pinCode;
+var typeofemail = typeof email;
+var typeofisMarried = typeof isMarried;
+
+console.log("MOBILE NO.7875547094, Type:", typeofmobileNumber);
+
+console.log("Pin Code:453632, Type:", typeofpinCode);
+
+console.log("Email:ambhore.asha26@gmail.com, Type:", typeofemail);
+
+console.log("ISMarried:YES, Type:", typeofisMarried);
+
+console.log("...............DIFFERENCE BETWEEN NULL AND UNDEFINE..........");
+
+var city = null;
+var permentaddress;
+
+var typeofcity = typeof city;
+var typeofpermentaddress = typeofpermentaddress;
+
+console.log("Type of city:",typeofcity);
+
+console.log("permentaddress:",typeofpermentaddress);
+
+
+
