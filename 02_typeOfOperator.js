5dacff6ee47d7279df61828641d8f21a90476bc5
ashadeshmukh <ambhore.asha26@gmail.com>
2023-02-18 13:21:27 +0530

    updated file 02_typeOfOperator
    updated file 02_typeOfOperator

---
+++ 02_typeOfOperator.js
@@ -0,0 +1,31 @@
+console.log("...........DECLARE AND INITLIZATION OF BANKNAME VARIABLE..............");
+var bankName;
+var bankName = 'SBI Bank';
+var typeofbankName = typeof bankName;
+console.log("Bank Name:SBI Bank, Type:",typeofbankName);
+
+console.log(".............TYPE OF OPERATOR MARKS..........");
+var marks;
+var marks ='90%';
+var typeofmarks = typeof marks;
+console.log("value of marks:",typeofmarks);
+console.log("Marks:90%, Type:",typeofmarks);
+
+
+console.log("..............DECLARE AND INITLIZATION OF ISworking VARIABLES............");
+var isWorking;
+var isWorking = true;
+var typeofisWorking = typeof isWorking;
+console.log("Type of value:",isWorking);
+console.log("working Type:", typeofisWorking);
+
+console.log("..............DECLAR AND INITLIZATION OF totalCount VARIABLE..............");
+var totalCount;
+var totalCount = 'Hundred and Seven';
+var typeoftotalCount = typeof totalCount;
+console.log("DataType:",typeoftotalCount);
+
+console.log("..........UPDATE VARIABLE TOTALCOUNT...............");
+var totalCount = 107;
+var typeoftotalCount = typeof totalCount;
+console.log("DATA TYPE of total COUNT:", typeoftotalCount);
