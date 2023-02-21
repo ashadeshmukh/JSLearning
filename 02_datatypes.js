b8f5c7f4c13f9f18ba39760ebbdc8c8f5fee050e
ashadeshmukh <ambhore.asha26@gmail.com>
2023-02-21 13:21:21 +0530

    update 02_ data types.js
    update 02_data types.js

--- 02_datatypes.js
+++ 02_datatypes.js
@@ -31,3 +31,12 @@ var temp = num1;
 num1 = num2;
 num2 = temp;
 console.log( num1, num2);
+
+console.log("swinning variables:");
+var str1 = "sweety";
+var str2 = "cutey";
+
+var temp = str1;
+str1 = str2;
+str2 = temp;
+console.log(str1,str2);  
