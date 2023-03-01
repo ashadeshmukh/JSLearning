29307039251fc606ec628d9ae55a2ee36a3caaae
ashadeshmukh <ambhore.asha26@gmail.com>
2023-03-01 13:17:11 +0530

    create file 06_for_Assignment01.js
    create file 06_for_Assignment01.js

---
+++ 06_for_Assignment01.js
@@ -0,0 +1,39 @@
+console.log(`print numbers from 5 to 15 by incrementing 1:`);
+for (let index = 5; index<=15;index= ++index) {
+    console.log(index);
+    
+}
+
+console.log(`PRINT NUMBER FROM 50 TO 40 BY decrementing by 1:`);
+for (let index = 50; index>=40; index= --index) {
+    console.log(index);
+    
+}
+
+console.log('Print Find first 15 odd numbers:');
+for (let index = 1; index<=30; index=index+2){
+      console.log(index); 
+}
+
+console.log('Print find first 10 even numbers:');
+for (let index = 2; index <=20; index=index+2) {
+    console.log(index);
+    
+}
+
+console.log(`Print table of 5:`);
+for (let index = 5; index <=50; index = index+5) {
+    console.log(index);
+   
+}
+
+console.log(`Print table of 10 like 10 20 30 .........100:`);
+for (let index = 10; index <=100; index=index+10){
+    console.log(index);
+}
+
+console.log(`Print of 10 in reverse order like 100 90 80.....10`);
+for (let index = 100; index >= 10; index=index-10 ){
+    console.log(index);
+
+}
